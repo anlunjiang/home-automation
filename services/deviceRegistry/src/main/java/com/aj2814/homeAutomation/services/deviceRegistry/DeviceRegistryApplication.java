a9227d86f49ae317c80867c3dafbd8863c2f6885
anlunjiang <anlunjiang@outlook.com>
2022-04-18 00:37:59 +0100

    bootstrap device registry spring boot application

---
+++ services/deviceRegistry/src/main/java/com/aj2814/homeAutomation/services/deviceRegistry/DeviceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.aj2814.homeAutomation.services.deviceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceRegistryApplication.class, args);
+	}
+
+}
