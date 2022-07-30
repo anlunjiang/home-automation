5493a4f0aa815f824667caa7b8dd83f9a60024ff
anlunjiang <anlunjiang@outlook.com>
2022-07-30 17:31:57 +0100

    application

---
+++ services/deviceControllers/src/main/java/com/aj2814/homeAutomation/services/deviceControllers/DeviceControllersApplication.java
@@ -0,0 +1,13 @@
+package com.aj2814.homeAutomation.services.deviceControllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceControllersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceControllersApplication.class, args);
+	}
+
+}
---
+++ services/deviceControllers/src/test/java/com/aj2814/homeAutomation/services/deviceControllers/DeviceControllersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aj2814.homeAutomation.services.deviceControllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceControllersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
